a0364216b1f36115b76aa5a83096f536227d9bec
Marianvsf <marianvsf@gmail.com>
2025-11-16 20:44:15 -0400

    invoices

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,12 @@
-import { fetchRevenue } from "../lib/data";
+import { fetchLatestInvoices, fetchRevenue } from "../lib/data";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import { lusitana } from "../ui/fonts";
 
 export default async function DashboardPage() {
     const revenue = await fetchRevenue();
-    console.log(revenue);
+    const latestInvoices = await fetchLatestInvoices();
+
     return <main>
         <h1 className={`${lusitana.className}text-2xl font-bold`}>Dashboard</h1>
         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
@@ -19,7 +21,7 @@ export default async function DashboardPage() {
         </div>
         <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
             <RevenueChart revenue={revenue} />
-            {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+            <LatestInvoices latestInvoices={latestInvoices} />
         </div>
     </main>;
 }
